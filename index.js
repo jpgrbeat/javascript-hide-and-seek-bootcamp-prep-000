28fcc8abb8131401cf7fff68f3457e0deb072f4f
Joshua R Miller <joshua.ross.miller@gmail.com>
2018-06-05 20:11:43 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,16 @@
+function getFirstSelector(selector){
+  return document.querySelector(selector);
+}
+function nestedTarget(){
+  return document.getElementById('nested').querySelector('.target')
+}
+function increaseRankBy(n){
+  const lis = document.getElementById('app').querySelectorAll('ul.ranked-list li');
+  for(let i = 0; i < lis.length; i++){
+    lis[i].innerHTML = (Number.parseInt(n)+i + 1).toString();
+  }
+  return lis;
+}
+function deepestChild(){
+  
+}
